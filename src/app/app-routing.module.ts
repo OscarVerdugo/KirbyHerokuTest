85ec104113103b9cb6f3013748c3bdfb5b826e67
OscarVerdugo <oscareduardover@gmail.com>
2020-12-03 23:10:14 -0700

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Route1Component } from './route1/route1.component';
+import { Route2Component } from './route2/route2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    pathMatch:"full",
+    redirectTo:"route1"
+  },
+  {
+    path:"route1",
+    component:Route1Component
+  },
+  {
+    path:"route2",
+    component:Route2Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
